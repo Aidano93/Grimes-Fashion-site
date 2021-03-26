2f3639c84274c2d08aec2f27c99dc10fdb89d154
Joel Kaye <kayej22@hotmail.com>
2021-03-26 13:27:55 -0600

    css update and small greensock update

--- public/js/greensock.js
+++ public/js/greensock.js
@@ -1,4 +1,4 @@
-gsap.to(".logo", {duration: 2, x: 300});
-gsap.to(".logotext", {duration: 2, x: 300});
+gsap.to(".logo", {duration: 2, x: 300, ease: "bounce"});
+gsap.to(".logotext", {duration: 2, x: 300, ease: "bounce"});
 
 

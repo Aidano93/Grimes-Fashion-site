2d42dfaa5c626c69f89a9b4b5dae5f9430bf70b7
Aidan OReilly <ajoreilly93@gmail.com>
2021-03-26 13:08:02 -0600

    updated seeds

--- seeds/member.js
+++ seeds/member.js
@@ -2,28 +2,28 @@ module.exports = [
   {
     name: 'Aidan',
     profilePic: 'images/aidan.jpg',
-    title: '',
+    title: 'Captain Dan',
     bio: 'Web Development student at SAIT ',
     github: 'https://github.com/Aidano93'
   },
   {
     name: 'Dustin',
     profilePic: 'images/dustin.jpg',
-    title: '',
+    title: 'The Duster',
     bio: 'Coding and customer service are my forte!',
     github: 'https://github.com/cowtowndusty'
   },
   {
     name: 'Joel',
     profilePic: 'images/joel.jpg',
-    title: '',
+    title: 'Grand Pubah',
     bio: 'Starting out my coding journey!',
     github: 'https://github.com/kayej22'
   },
   {
     name: 'Makayla',
     profilePic: 'images/makayla.jpg',
-    title: '',
+    title: 'CSS extraordinaire',
     bio: 'Current student at SAIT taking the Web Developing Program. If you need a beautiful and accessible website, I\'m your gal.',
     github: 'https://github.com/MakaylaMR'
   },

aa26f87f7403569902cd2663a7d72711c7a80786
Joel Kaye <kayej22@hotmail.com>
2021-03-26 14:36:27 -0600

    pushing changes

--- public/js/greensock.js
+++ public/js/greensock.js
@@ -1,4 +1,5 @@
-gsap.to(".logo", {duration: 2, x: 300, ease: "bounce"});
-gsap.to(".logotext", {duration: 2, x: 300, ease: "bounce"});
+// gsap.from(".logo", {duration: 2, scale: 0.3, ease: "bounce"});
+
+// gsap.to(".logotext", {duration: 2, x: 300, ease: "bounce"});
 
 

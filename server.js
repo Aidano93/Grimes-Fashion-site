642d9b5bf96c7b0d27fdf9fcae5e725d177fabc8
Aidan OReilly <ajoreilly93@gmail.com>
2021-03-26 15:28:22 -0600

    added error handling and blank error page

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', index)
 app.use('/verify-sub', verifySub)
 
+// 404
+app.use((req, res, next) => {
+  const err = new Error('Requested page could not be found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const errStatus = err.status || 500;
+  res.locals.status = errStatus;
+  res.status(errStatus);
+  // General error page rendered. specific error code set by ejs variables
+  res.render('pages/error', {
+    pageTitle: errStatus, 
+    errCode: errStatus
+  });
+});
 
 //port set via .env file or default 3000
 const port = process.env.PORT || 3000;

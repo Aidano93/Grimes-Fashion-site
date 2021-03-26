4c35cd0053adfc9bfb2abb76fb4a8986dc9f88be
Aidan OReilly <ajoreilly93@gmail.com>
2021-03-26 13:12:40 -0600

    conflict confusion

--- seeds/member.js
+++ seeds/member.js
@@ -9,33 +9,21 @@ module.exports = [
   {
     name: 'Dustin',
     profilePic: 'images/dustin.jpg',
-<<<<<<< HEAD
-    title: 'The Duster',
-=======
     title: 'Tony\'s Fantastic Four',
->>>>>>> cd487b54c4f59569b1a09e4a1ca22370de7c447f
     bio: 'Coding and customer service are my forte!',
     github: 'https://github.com/cowtowndusty'
   },
   {
     name: 'Joel',
     profilePic: 'images/joel.jpg',
-<<<<<<< HEAD
-    title: 'Grand Pubah',
-=======
     title: 'Tony\'s Fantastic Four',
->>>>>>> cd487b54c4f59569b1a09e4a1ca22370de7c447f
     bio: 'Starting out my coding journey!',
     github: 'https://github.com/kayej22'
   },
   {
     name: 'Makayla',
     profilePic: 'images/makayla.jpg',
-<<<<<<< HEAD
-    title: 'CSS extraordinaire',
-=======
     title: 'Tony\'s Fantastic Four',
->>>>>>> cd487b54c4f59569b1a09e4a1ca22370de7c447f
     bio: 'Current student at SAIT taking the Web Developing Program. If you need a beautiful and accessible website, I\'m your gal.',
     github: 'https://github.com/MakaylaMR'
   },

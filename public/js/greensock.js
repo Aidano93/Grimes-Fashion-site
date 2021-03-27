ff194a147c66ba25f32bb9bc004c47b202e0762c
Joel Kaye <kayej22@hotmail.com>
2021-03-26 18:49:05 -0600

    animation tweeks and home page rework

--- public/js/greensock.js
+++ public/js/greensock.js
@@ -1,5 +1,16 @@
-// gsap.from(".logo", {duration: 2, scale: 0.3, ease: "bounce"});
 
-// gsap.to(".logotext", {duration: 2, x: 300, ease: "bounce"});
+gsap.fromTo(".logo", 2.3, {autoAlpha: 0}, {autoAlpha: 1, immediateRender:false})
+gsap.from(".logo", {duration: 2, scale: 0.3, ease: "bounce"});
+
+gsap.fromTo(".logotext", 2.3, {autoAlpha: 0}, {autoAlpha: 1, immediateRender:false})
+gsap.from(".logotext", {duration: 2, scale: 0.3, opacity: 100, delay: .2, ease: "bounce"});
 
 
+// const $el = document.querySelector('.fade-in');
+// const duration = 2;
+// const from = { opacity: 0, ease: Linear.ease };
+// const to = { opacity: 1 };
+
+// function fadeIn() {
+//    TweenLite.fromTo($el, duration, from, to);
+// }

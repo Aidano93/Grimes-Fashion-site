06779620165e89c08130de89922a4e9b23bff6cb
Joel Kaye <kayej22@hotmail.com>
2021-03-26 19:19:09 -0600

    pushed latest changes

--- public/js/greensock.js
+++ public/js/greensock.js
@@ -5,12 +5,6 @@ gsap.from(".logo", {duration: 2, scale: 0.3, ease: "bounce"});
 gsap.fromTo(".logotext", 2.3, {autoAlpha: 0}, {autoAlpha: 1, immediateRender:false})
 gsap.from(".logotext", {duration: 2, scale: 0.3, opacity: 100, delay: .2, ease: "bounce"});
 
+gsap.fromTo(".tagline", 1, {autoAlpha: 0,}, {autoAlpha: 1, immediateRender:false})
+gsap.from(".tagline", {duration: 2, x: 300, delay: 1, ease: "bounce"});
 
-// const $el = document.querySelector('.fade-in');
-// const duration = 2;
-// const from = { opacity: 0, ease: Linear.ease };
-// const to = { opacity: 1 };
-
-// function fadeIn() {
-//    TweenLite.fromTo($el, duration, from, to);
-// }

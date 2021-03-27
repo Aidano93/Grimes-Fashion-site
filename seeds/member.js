e485a3bbb976a52e7b3dd0785f804a40607ba237
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-26 23:09:00 -0600

    tweaks

--- seeds/member.js
+++ seeds/member.js
@@ -15,7 +15,7 @@ module.exports = [
   },
   {
     name: 'Joel',
-    profilePic: 'images/IMG_0024.JPG',
+    profilePic: 'images/IMG_0024.jpg',
     title: 'Tony\'s Fantastic Four',
     bio: 'Starting out my coding journey!',
     github: 'https://github.com/kayej22'
